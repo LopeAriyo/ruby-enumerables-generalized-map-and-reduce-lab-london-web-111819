ae40a04eb847047b7ff3ae35a13386d440f2d159
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 16:24:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+# My Code here....
+
+def map(array)
+  array.map{|n| n*-1}
+  array.map{|n| n}
+  array.map{|n| n*2}
+  array.map{|n| n**2 }
+end
+
+def reduce(array, starting_point)
+
+
+
+end
