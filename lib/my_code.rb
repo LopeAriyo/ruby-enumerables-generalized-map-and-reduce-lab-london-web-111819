62664a67e26eb88b87ac4d6d999a060f08ef46fb
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 16:52:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,18 +10,12 @@ def map(array)
 
     result.push(yield(array[i]))
     i += 1
-    
+
   end
 
 p result
 
 end
-
-#array.map{|num| num *-1}
-#array.map{|num| num}
-#array.map{|num| num*2}
-#array.map{|num| num**2 }
-
 
 #using yield to pass an argument
 
